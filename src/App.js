ee8190569c303b87e249345c964fcee0cbd701d2
nhukhanhng <ngkhanh14.kn@gmail.com>
2021-12-29 09:46:39 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Quote from "./Components/Quote.js";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [quote, setQuote] = useState({
+    anime: null,
+    character: null,
+    quote: null,
+  });
+
+  const fetchQuote = async () => {
+    return await fetch('https://animechan.vercel.app/api/random')
+      .then(response => response.json());
+  }
+
+  const generate = async () => {
+    setQuote(await fetchQuote());
+  }
+
+  useEffect(async () => {
+    setQuote(await fetchQuote());
+  }, []);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="shimmer">Rendamei</h1>
+        <h2>Anime Quote Random Generator</h2>
+          <Quote quote={quote}/>
+          <button onClick={generate}>Generate new quote</button>
     </div>
   );
 }
